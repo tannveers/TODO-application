da6876b1b07382064a8fe29ffadaf75422cf017a
Tanveer Singh <tanveer9812@gmail.com>
2020-06-11 22:35:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import Todos from "./components/Todos";
+import Header from "./components/layout/header";
+import AddTodo from "./components/addTodo";
+import { v1 as uuid } from "uuid";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./components/pages/About";
+import axios from "axios";
+
+class App extends Component {
+  state = {
+    todos: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => this.setState({ todos: res.data }));
+  }
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos
+                  todos={this.state.todos}
+                  markComplete={this.markComplete}
+                  delTodo={this.delTodo}
+                />
+              </React.Fragment>
+            )}
+          />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
