81463dce20a626ab474c6a4dec9bcaae1cf05bbd
Tanveer Singh <tanveer9812@gmail.com>
2020-06-11 22:38:05 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>
+        <p>This is a todo-list app version 1.0.0</p>
+      </h1>
+    </React.Fragment>
+  );
+}
+
+export default About;
